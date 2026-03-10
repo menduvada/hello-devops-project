d404897740252be275c6b631fe9b838ff78f195b
Venisha Patel <papa@Virajs-MacBook-Pro.local>
2026-03-10 23:51:29 +0530

    Initial DevOps Hello World Project

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static String sayHello() {
+        return "Hello DevOps World";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sayHello());
+    }
+}
